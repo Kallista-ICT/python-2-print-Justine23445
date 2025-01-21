fe980669595ed792b6fb54c258947109a733d6c5
Justine23445 <justinecallysta@gmail.com>
2025-01-21 07:27:22 +0000

    Submit assigment ##

---
+++ Your_Future/future.py
@@ -0,0 +1,7 @@
+print("welcome to know your age in ten years ")
+name = input("hi what is your name ")
+age = int(input("enter your current age here ") )
+
+# this is a comment 
+print(f"hello {name}, in ten years you'll be {age+ 10}")# its easy to understand and simple and efficient 
+
